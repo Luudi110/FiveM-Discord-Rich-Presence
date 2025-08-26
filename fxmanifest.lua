fbbabc5e49004606b99c147381f6ea7d64d07c0f
Luudi_110 <ludwig@ejkens.dk>
2025-08-26 16:17:21 +0200

    Created fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'rdr3'
+
+author 'YourName / Discord Rich Presence for RedM'
+description 'Configurable Discord Rich Presence in pure Lua for RedM'
+version '1.0.0'
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources may become incompatible.'
+
+lua54 'yes'
+
+client_scripts {
+	'config.lua',
+	'client.lua'
+}
+
+
